9fd0aa10001f93e2c64a80ed6f5dd3469ab52152
valdesin <vlad19943103@gmail.com>
2017-10-07 14:30:31 +0200

    Update Lesson package

---
+++ 02_package/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/valdesin/udemyTraining/02_package/icomefromealaska"
+	"github.com/valdesin/udemyTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(winniepooh.BearName)
+	fmt.Println(stringutil.MyName)
+	fmt.Println(stringutil.Resverse("!oG olleH"))
+}
